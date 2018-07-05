af0a5d8c1955f46007143eb637014fffa7edfc72
Cristian Garcia <cristiangarcia@Cristians-MacBook-Pro.local>
2018-07-05 20:53:46 -0300

    add button callback

--- app/src/main/java/test/co/cristiangarcia/testapp/MainActivity.java
+++ app/src/main/java/test/co/cristiangarcia/testapp/MainActivity.java
@@ -1,7 +1,10 @@
 package test.co.cristiangarcia.testapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import test.co.cristiangarcia.testapp.lib.Services;
@@ -12,12 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context context= this;
+        final Services service= new Services();
 
-        Services service= new Services();
+        final TextView response = (TextView) findViewById(R.id.response);
 
-        TextView response = (TextView) findViewById(R.id.response);
-
-        service.showMessage(this,response);
+        Button getMessage= (Button) findViewById(R.id.getMessage);
+        getMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                service.showMessage(context,response);
+            }
+        });
 
     }
 }
